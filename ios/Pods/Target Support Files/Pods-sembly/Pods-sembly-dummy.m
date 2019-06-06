df8ac7489af66443c0fb44e9c176903f4f86cd91
nicollash <ehlers.nicollas@gmail.com>
2019-06-06 09:57:32 -0400

    re-applied gitignore

--- ios/Pods/Target Support Files/Pods-sembly/Pods-sembly-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_sembly : NSObject
-@end
-@implementation PodsDummy_Pods_sembly
-@end
--- ios/Pods/Target Support Files/Pods-semblyTests/Pods-semblyTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_semblyTests : NSObject
-@end
-@implementation PodsDummy_Pods_semblyTests
-@end

f9cad2de258255d9b86688c7e1a709f3d7d73c1f
nicollash <ehlers.nicollas@gmail.com>
2019-09-11 14:55:34 -0400

    updated newPost

--- vendor/firebase/functions/index.js
+++ vendor/firebase/functions/index.js
@@ -1,7 +1,7 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const util = require('util');
-admin.initializeApp(functions.config().firebase);
+admin.initializeApp();
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -11,8 +11,10 @@ exports.newPost = functions.https.onRequest((request, response) => {
     admin.firestore().collection('Posts').add(request.body)
     .then(function(docRef) {
         console.log("Document written with ID: ", docRef.id);
+        return true;
     })
     .catch(function(error) {
         console.error("Error adding document: ", error);
+        return false;
     });
 });

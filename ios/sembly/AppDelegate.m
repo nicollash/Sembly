4a5b6ae6ab95f51556a8f11d2a2df835893b6e6e
nicollash <ehlers.nicollas@gmail.com>
2019-09-17 12:25:59 -0400

    Fixed appdelegate

--- ios/sembly/AppDelegate.m
+++ ios/sembly/AppDelegate.m
@@ -23,7 +23,7 @@
                                                    moduleName:@"sembly"
                                             initialProperties:nil];
   
-  Thread.sleep(forTimeInterval: 2.0)
+  [NSThread sleepForTimeInterval:2.0];
   
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 

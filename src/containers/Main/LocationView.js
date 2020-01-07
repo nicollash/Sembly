704901b97ca6f030a0234f8ee79db67429786f93
nicollash <ehlers.nicollas@gmail.com>
2020-01-07 18:24:16 -0500

    Fix white square layout issue in LocationView

--- src/containers/Main/LocationView.js
+++ src/containers/Main/LocationView.js
@@ -48,6 +48,7 @@ class LocationView extends React.Component {
   }
 
   componentDidMount() {
+    console.log(this.props.panelHeight);
   }
 
   render() {
@@ -64,158 +65,155 @@ class LocationView extends React.Component {
     const phoneNumber = location.phone ? parsePhoneNumberFromString(location.phone) : undefined;
 
     return (
-      <View>
-        <View style={{ height: (this.props.panelHeight > 50 ? this.props.panelHeight : 400) }}>
-          <ScrollView>
-            <View style={{
-              justifyContent: 'flex-start',
-              alignItems: 'center',
-              borderRadius: 12,
-            }}
-            >
-              <View style={{ width: '100%' }}>
-                <View style={{ minHeight: 190, width: '100%' }}>
-                  <ImageBackground
-                    style={{ flex: 1 }}
-                    source={{ uri: location.picture } || require('../../../assets/images/SemblyLogo.png')}
-                  />
-                </View>
-                <View style={{ marginTop: 10 }}>
-                  <View style={{
-                    flexDirection: 'row',
-                    alignItems: 'center',
-                    width: '96%',
-                    marginTop: 1,
+      <ScrollView>
+        <View style={{
+          justifyContent: 'flex-start',
+          alignItems: 'center',
+          borderRadius: 12,
+        }}
+        >
+          <View style={{ width: '100%' }}>
+            <View style={{ minHeight: 190, width: '100%' }}>
+              <ImageBackground
+                style={{ flex: 1 }}
+                source={{ uri: location.picture } || require('../../../assets/images/SemblyLogo.png')}
+              />
+            </View>
+            <View style={{ marginTop: 10 }}>
+              <View style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                width: '96%',
+                marginTop: 1,
+              }}
+              >
+                <TouchableOpacity
+                  onPress={() => this.props.navigation.navigate('Feed')}
+                  style={{ marginLeft: wp(4) }}
+                  hitSlop={{
+                    bottom: 10, top: 5, right: 5, left: 5,
                   }}
-                  >
-                    <TouchableOpacity
-                      onPress={() => this.props.navigation.navigate('Feed')}
-                      style={{ marginLeft: wp(4) }}
-                      hitSlop={{
-                        bottom: 10, top: 5, right: 5, left: 5,
-                      }}
-                    >
-                      <Image source={require('../../../assets/images/PostViewGoBackButton.png')} />
-                    </TouchableOpacity>
-                    <Text
-                      style={{
-                        marginLeft: wp(3),
-                        fontSize: 22,
-                        color: '#000',
-                        fontFamily: Theme.fonts.bold,
-                        maxWidth: wp(82),
-                        textAlign: 'center',
-                      }}
-                    >
-                      {location.title ? location.title : location.name}
-                    </Text>
-                  </View>
-                  <View style={{ marginTop: 3 }}>
+                >
+                  <Image source={require('../../../assets/images/PostViewGoBackButton.png')} />
+                </TouchableOpacity>
+                <Text
+                  style={{
+                    marginLeft: wp(3),
+                    fontSize: 22,
+                    color: '#000',
+                    fontFamily: Theme.fonts.bold,
+                    maxWidth: wp(82),
+                    textAlign: 'center',
+                  }}
+                >
+                  {location.title ? location.title : location.name}
+                </Text>
+              </View>
+              <View style={{ marginTop: 3 }}>
+                <Text style={{
+                  marginLeft: wp(10),
+                  width: '87%',
+                  fontSize: wp(3.5),
+                  fontFamily: Theme.fonts.regular,
+                  color: '#000',
+                }}
+                >
+                  {location.text || location.about}
+                </Text>
+              </View>
+              <View style={{
+                flexDirection: 'row',
+                marginLeft: wp(10),
+                alignItems: 'center',
+              }}
+              >
+                <TouchableOpacity
+                  onPress={() => this.props.updateMap(location.location.lat, location.location.lon)}
+                >
+                  <View style={{ flexDirection: 'row', paddingVertical: hp(1) }}>
+                    {location.location.name !== '' && (
+                      <Image source={require('../../../assets/images/LocationViewLocationPin.png')} />
+                    )}
                     <Text style={{
-                      marginLeft: wp(10),
-                      width: '87%',
-                      fontSize: wp(3.5),
-                      fontFamily: Theme.fonts.regular,
-                      color: '#000',
+                      fontSize: wp(3.3),
+                      color: 'black',
+                      marginLeft: wp(1),
+                      fontFamily: Theme.fonts.bold,
                     }}
                     >
-                      {location.text || location.about}
+                      {location.location.name}
                     </Text>
                   </View>
-                  <View style={{
-                    flexDirection: 'row',
-                    marginLeft: wp(10),
-                    alignItems: 'center',
-                  }}
+                </TouchableOpacity>
+                {location.className === 'Business' && (
+                  <TouchableOpacity
+                    onPress={() => { Linking.openURL(`telprompt:${location.phoneNumber}`); }}
                   >
-                    <TouchableOpacity
-                      onPress={() => this.props.updateMap(location.location.lat, location.location.lon)}
-                    >
-                      <View style={{ flexDirection: 'row', paddingVertical: hp(1) }}>
-                        {location.location.name !== '' && (
-                          <Image source={require('../../../assets/images/LocationViewLocationPin.png')} />
-                        )}
-                        <Text style={{
-                          fontSize: wp(3.3),
-                          color: 'black',
-                          marginLeft: wp(1),
-                          fontFamily: Theme.fonts.bold,
-                        }}
-                        >
-                          {location.location.name}
-                        </Text>
-                      </View>
-                    </TouchableOpacity>
-                    {location.className === 'Business' && (
-                      <TouchableOpacity
-                        onPress={() => { Linking.openURL(`telprompt:${location.phoneNumber}`); }}
-                      >
-                        <View style={{ flexDirection: 'row', marginLeft: 5 }}>
-                          {location.phone !== '' && (
-                            <Image source={require('../../../assets/images/LocationViewPhoneIcon.png')} />
-                          )}
-                          <Text
-                            style={{
-                              fontSize: wp(3.3),
-                              color: '#000',
-                              fontFamily: Theme.fonts.bold,
-                              marginLeft: 3,
-                            }}
-                          >
-                            {phoneNumber ? phoneNumber.formatNational() : location.phone}
-                          </Text>
-                        </View>
-                      </TouchableOpacity>
-                    )}
-                    {location.constructor.name === 'Event' && (
-                      <View style={{ flexDirection: 'row', marginLeft: wp(1.5), alignItems: 'center' }}>
-                        <Image
-                          source={require('../../../assets/images/clockIcon.png')}
-                          style={{ height: 15, resizeMode: 'contain' }}
-                        />
-                        <Text style={{
+                    <View style={{ flexDirection: 'row', marginLeft: 5 }}>
+                      {location.phone !== '' && (
+                        <Image source={require('../../../assets/images/LocationViewPhoneIcon.png')} />
+                      )}
+                      <Text
+                        style={{
                           fontSize: wp(3.3),
                           color: '#000',
                           fontFamily: Theme.fonts.bold,
-                          marginLeft: wp(0.5),
+                          marginLeft: 3,
                         }}
-                        >
-                          {location.happeningOn.format('MMMM Do YYYY, h:mm:ss a')}
-                        </Text>
-                      </View>
-                    )}
-                  </View>
-                </View>
-              </View>
-              <View style={styles.separatorBar} />
-              <View style={{ left: '2.8%', marginTop: isIphoneX() ? hp(1) : hp(1), width: '100%' }}>
-                <FlatList
-                  data={this.props.posts}
-                  renderItem={({ item }) => (
-                    <FeedUserPost
-                      post={item}
-                      postID={item.id}
-                      location={item.title}
-                      username={item.user.name}
-                      userPostText={item.text}
-                      userPostPicture={item.picture}
-                      userProfilePicture={item.user.avatar}
-                      moveOnPress={() => this.props.navigation.navigate('Post', { post: item, sourceLocation: location })}
-                      comments={item.comments.length}
+                      >
+                        {phoneNumber ? phoneNumber.formatNational() : location.phone}
+                      </Text>
+                    </View>
+                  </TouchableOpacity>
+                )}
+                {location.constructor.name === 'Event' && (
+                  <View style={{ flexDirection: 'row', marginLeft: wp(1.5), alignItems: 'center' }}>
+                    <Image
+                      source={require('../../../assets/images/clockIcon.png')}
+                      style={{ height: 15, resizeMode: 'contain' }}
                     />
-                  )}
-                  ItemSeparatorComponent={() => (
-                    <View style={{ height: 8 }} />
-                  )}
-                  ListFooterComponent={() => (
-                    <View style={{ height: 100 }} />
-                  )}
-                />
+                    <Text style={{
+                      fontSize: wp(3.3),
+                      color: '#000',
+                      fontFamily: Theme.fonts.bold,
+                      marginLeft: wp(0.5),
+                    }}
+                    >
+                      {location.happeningOn.format('MMMM Do YYYY, h:mm:ss a')}
+                    </Text>
+                  </View>
+                )}
               </View>
             </View>
-          </ScrollView>
+          </View>
+          <View style={styles.separatorBar} />
+          <View style={{ left: '2.8%', marginTop: isIphoneX() ? hp(1) : hp(1), width: '100%' }}>
+            <FlatList
+              data={this.props.posts}
+              renderItem={({ item }) => (
+                <FeedUserPost
+                  post={item}
+                  postID={item.id}
+                  location={item.title}
+                  username={item.user.name}
+                  userPostText={item.text}
+                  userPostPicture={item.picture}
+                  userProfilePicture={item.user.avatar}
+                  moveOnPress={() => this.props.navigation.navigate('Post', { post: item, sourceLocation: location })}
+                  comments={item.comments.length}
+                />
+              )}
+              ItemSeparatorComponent={() => (
+                <View style={{ height: 8 }} />
+              )}
+              ListFooterComponent={() => (
+                <View style={{ height: 100 }} />
+              )}
+            />
+          </View>
         </View>
-      </View>
+        <View style={{ height: this.props.panelHeight === 20 ? 190 : hp(100) - 80 - this.props.panelHeight }} />
+      </ScrollView>
     );
   }
 }
